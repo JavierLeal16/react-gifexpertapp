73731e2d88285183ef933ce8eedcf85855c0f945
Javier Leal <JavieraLealG@gmail.com>
2020-12-12 20:13:19 -0400

    HTTPS actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async ( categoria) =>{
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=5&api_key=vqK0l4lu4cQPEkS2txke4LjprVoiCopL`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=5&api_key=vqK0l4lu4cQPEkS2txke4LjprVoiCopL`;
 
     const respuesta = await fetch ( url );
     const { data } = await respuesta.json();
